544e1ab9572aa8387617b83e8a441e0ebc7422d5
gueejla <gueejla@gmail.com>
2026-09-10 13:22:44 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# A notes app that is fully local and makes maximum use of the security and local-first features available
+
---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>
+            Hello world?
+          </h1>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
